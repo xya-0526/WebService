f4cf3b10c0bb8e50c445d05373561a8269ef3dd0
xya-526 <468422680@qq.com>
2025-10-04 23:35:16 +0800

    修改bug3

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,8 @@
-
-# Build stage
-FROM node:20-alpine AS builder
+FROM node:20-alpine
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY . .
 RUN npm run build
-
-# Production stage
-FROM node:20-alpine
-WORKDIR /app
-COPY package*.json ./
-RUN npm install --production
-COPY --from=builder /app/dist ./dist
 EXPOSE 3000
 CMD ["node", "dist/main.js"]
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,10 +8,12 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
   @Patch('/updata')
   update(@Request() req) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     return this.userService.update(req.user.phoneNumber, req.body);
   }
   @Patch('/changePassword')
   changePassword(@Request() req) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     return this.userService.changePassword(req.user.phoneNumber, req.body);
   }
 }
